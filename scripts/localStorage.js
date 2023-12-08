8c22b6320ca6614ee8d48dcd4b4e8766de9735b1
Alejandro Garcia <jandro_es@hotmail.com>
2023-12-08 12:08:52 +0100

    Vamos realizando cambios en el productManager.js

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,6 +1,6 @@
-export function storeProducts(defaultProducts) {
+export function storeProducts(productList) {
 
-    defaultProducts.forEach(product => {
+  productList.forEach(product => {
       if (product.nombre && product.autor && product.cantidad && product.precio) {
         const plainProduct = { id: product.id, nombre: product.nombre, autor: product.autor, cantidad: product.cantidad, precio: product.precio };
         const productJson = JSON.stringify(plainProduct);
--- scripts/productManager.js
+++ scripts/productManager.js
@@ -5,49 +5,134 @@ export class ProductManager {
         this.#products = [];
     }
 
-    // Método para obtener la lista de productos
-    listProducts() {
-        return this.#products;
+    // Agrega un producto al inventario
+  addProduct(product) {
+    this.products.push(product);
+    if (product.nombre && product.autor && product.cantidad && product.precio) {
+      const plainProduct = {
+        id: product.id,
+        nombre: product.name,
+        autor: product.autor,
+        cantidad: product.quantity,
+        precio: product.price,
+      };
+      const productJson = JSON.stringify(plainProduct);
+      localStorage.setItem(`Producto: ${product.id}`, productJson);
     }
 
-    // Método para agregar el producto
-    addProduct(product) {
-        this.#products.push(product);
+    this.displayInventory();
+  }
+
+// Edita un producto en el inventario
+  editProduct(id) {
+    let product = this.products.find((product) => product.id === id);
+    if (product) {
+      const newName = prompt("Introduce el nuevo nombre:", product.nombre);
+      const newAuthor = prompt("Introduce el nuevo autor:", product.autor);
+      const newQuantity = prompt("Introduce la nueva cantidad:", product.cantidad);
+      const newPrice = prompt("Introduce el nuevo precio:", product.precio);
+  
+      if (newName !== null && newAuthor !== null && newQuantity !== null && newPrice !== null) {
+        this.updateProduct(id, newName, newAuthor, newQuantity, newPrice);
+        this.displayInventory();
+      }
+    }
+  }
+// Elimina un producto del inventario
+  deleteProduct(id) {
+    this.products = this.products.filter((product) => product.id !== id);
+    localStorage.removeItem(`Producto: ${id}`);
+    this.displayInventory();
+  }
+// Actualiza la información de un producto en el inventario
+  updateProduct(id, nombre, autor, cantidad, precio) {
+    let product = this.products.find((product) => product.id == id);
+    localStorage.removeItem(`Producto: ${id}`);
+
+    if (product) {
+      product.nombre = nombre;
+      product.autor = autor;
+      product.cantidad = cantidad;
+      product.precio = precio;
+      this.displayInventory();
     }
 
-    // Método para actualizar un producto por su ID
-    updateProductById(id, updateProduct) {
-        const index = this.#products.findIndex(product => product.id === id);
-
-        // Si no existe error (es coincidente el index)
-        if (index !== -1) {
-            this.#products[index] = updateProduct;
-        }
-
+    if (product.nombre && product.autor && product.cantidad && product.precio) {
+      const plainProduct = {
+        id: product.id,
+        nombre: product.nombre,
+        autor: product.autor,
+        cantidad: product.cantidad,
+        precio: product.precio,
+      };
+      const productJson = JSON.stringify(plainProduct);
+      localStorage.setItem(`Producto: ${product.id}`, productJson);
     }
+  }
+// Busca productos en el inventario por nombre
+  searchProduct(name) {
+    let filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().includes(name.toLowerCase())
+    );
+    this.displayInventory(filteredProducts);
+  }
+// Muestra los productos del inventario en la interfaz
+  displayInventory(products = this.products) {
+    const tableBody = document.getElementById("add-rows");
+    tableBody.innerHTML = "";
 
-    // Método para eliminar un producto
-    deleteProductById(id) {
-        const index = this.#products.findIndex(product => product.id === id);
+    products.forEach((product) => {
+      const row = this.createRow(product);
+      tableBody.appendChild(row);
+    });
+  }
 
-        if (index !== -1) {
-            this.#products.splice(index, 1);
-        }
-    }
+// Crea una fila para un producto en la interfaz
+  createRow(product) {
+    const row = document.createElement("tr");
+    const cellNames = this.createCell(product.name);
+    const cellQuantity = this.createCell(product.quantity);
+    const cellPrice = this.createCell(product.price);
+    const cellActions = this.createActionsCell(product.id);
 
-    // Método para mostrar todos los productos dentro del Array
-    showProducts() {
-        for (const product of this.#products) {
-            console.log(`ID: ${product.id}, Nombre: ${product.nombre}, Autor: ${this.autor}, Cantidad: ${product.cantidad}, Precio: ${product.precio}`);
-        }
-    }
+    row.appendChild(cellNames);
+    row.appendChild(cellQuantity);
+    row.appendChild(cellPrice);
+    row.appendChild(cellActions);
 
-    get products(){
-        return this.#products;
-    }
+    return row;
+  }
+// Crea una celda con un valor para la interfaz
+  createCell(value) {
+    const cell = document.createElement("td");
+    cell.textContent = value;
+    return cell;
+  }
+// Crea la celda de acciones (botones) para la interfaz
+  createActionsCell(productId) {
+    const cell = document.createElement("td");
+    const deleteButton = this.createButton("delete-button", "Borrar", () => this.deleteProduct(productId));
+    const editButton = this.createButton("edit-button", "Editar", () => this.editProduct(productId));
 
-    set products(value){
-        this.#products = value;
-    }
+    cell.appendChild(deleteButton);
+    cell.appendChild(editButton);
 
+    return cell;
+  }
+// Crea un botón con un manejador de eventos para la interfaz
+  createButton(className, text, clickHandler) {
+    const button = document.createElement("button");
+    button.className = className;
+    button.textContent = text;
+    button.addEventListener("click", clickHandler);
+    return button;
+  }
+// Obtiene la lista de productos
+  get products() {
+    return this.#products;
+  }
+// Establece la lista de productos
+  set products(products) {
+    this.#products = products;
+  }
 }
